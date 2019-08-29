2c2451316cb2ab6fc2a1597d093ea4b539bd2b44
imsagarj <jadhavsagar698@gmail.com>
2019-08-29 17:54:35 +0530

    model add

--- pawtectProject/pawtectApp/views.py
+++ pawtectProject/pawtectApp/views.py
@@ -71,43 +71,44 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
     
-def register(request):
-    registered = False
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileInfoForm(data=request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-            registered = True
-        else:
-            print(user_form.errors,profile_form.errors)
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileInfoForm()
-    return render(request,'pawtectApp/signup.html',
-                          {'user_form':user_form,
-                           'profile_form':profile_form,
-                           'registered':registered})
+# def register(request):
+#     registered = False
+#     if request.method == 'POST':
+#         user_form = UserForm(data=request.POST)
+#         profile_form = UserProfileInfoForm(data=request.POST)
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user = user_form.save()
+#             user.set_password(user.password)
+#             user.save()
+#             profile = profile_form.save(commit=False)
+#             profile.user = user
+#             profile.save()
+#             registered = True
+#         else:
+#             print(user_form.errors,profile_form.errors)
+#     else:
+#         user_form = UserForm()
+#         profile_form = UserProfileInfoForm()
+#     return render(request,'pawtectApp/signup.html',
+#                           {'user_form':user_form,
+#                            'profile_form':profile_form,
+#                            'registered':registered})
 def login(request):
-    form = UserForm()
-    if request.method == 'POST':
-        username = request.POST.get('username','')
-        password = request.POST.get('password','')
+    return render(request, 'pawtectApp/login.html')
+    # form = UserForm()
+    # if request.method == 'POST':
+    #     username = request.POST.get('username','')
+    #     password = request.POST.get('password','')
         
-        user =auth.authenticate(username=username, password=password)
-        if user:
-            auth.login(request, user)
-            return HttpResponseRedirect(reverse('plans'))
-        else:
-            messages.error(request,"Something went wrong try again.")
-            return HttpResponseRedirect(reverse('login'))
-    else:
-        return render(request, 'pawtectApp/login.html')
+    #     user =auth.authenticate(username=username, password=password)
+    #     if user:
+    #         auth.login(request, user)
+    #         return HttpResponseRedirect(reverse('plans'))
+    #     else:
+    #         messages.error(request,"Something went wrong try again.")
+    #         return HttpResponseRedirect(reverse('login'))
+    # else:
+    #     return render(request, 'pawtectApp/login.html')
 
 
 
