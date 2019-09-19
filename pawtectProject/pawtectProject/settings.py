630f9d8db6f57d13562aaadeb8dccef7ac9209b3
madhuriDesai <madhuri@bakedmoon.studio>
2019-09-19 13:03:07 +0530

    setting file add

--- pawtectProject/pawtectProject/settings.py
+++ pawtectProject/pawtectProject/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'pawtectProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pawtect',
+        'NAME': 'pawtectapp',
         'USER': 'pawtect',
         'PASSWORD': 'pawtect',
         'HOST': 'localhost',
